a3f97214fcbd4267ba9bf95f4048f36c7dac4afe
yohann renouard <yohann.renouard@gmail.com>
2024-06-10 22:16:49 +0200

    FirstPokemonCard

---
+++ src/components/PokémonCard.jsx
@@ -0,0 +1,13 @@
+function PokémonCard() {
+    return (
+        <div>
+            <h1>Pokemon Card</h1>;
+            <figure>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+                <figcaption>Bulbasaur</figcaption>
+            </figure>
+        </div>
+    );
+}
+
+export default PokémonCard;
