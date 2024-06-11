7caf040080538b1add5354fc1fcc25dd5134795c
yohann renouard <yohann.renouard@gmail.com>
2024-06-11 10:08:02 +0200

    pokemonlist

--- src/components/PokémonCard.jsx
+++ src/components/PokémonCard.jsx
@@ -1,13 +1,42 @@
+const pokemonList = [
+
+    {
+
+        name: "bulbasaur",
+
+        imgSrc:
+
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+    },
+
+    {
+
+        name: "mew",
+
+    },
+
+];
+
 function PokémonCard() {
+    const pokemon = pokemonList[0];
     return (
         <div>
-            <h1>Pokemon Card</h1>;
+            <h1>{pokemon.name}</h1>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-                <figcaption>Bulbasaur</figcaption>
+                {pokemon.imgSrc ? (
+                    <img src={pokemon.imgSrc} alt={pokemon.name} />
+                ) : (
+                    <p>???</p>
+                )}
             </figure>
         </div>
     );
 }
 
+
 export default PokémonCard;
+
+{/* <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" /> */ }
+
+// 
